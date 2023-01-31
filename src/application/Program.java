3cb6bd545941afce94d4d7dcc4ce6f781058dd8a
Leandro Souza Oliveira <leandro.oliveirasouza1@gmail.com>
2023-01-31 19:27:27 -0300

    TaxPayer calculation and Program report

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,18 +10,19 @@ import entities.TaxPayer;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Quantos contribuintes você vai digitar? ");
 		int n = sc.nextInt();
-		
+
 		List<TaxPayer> list = new ArrayList<>();
-		
-		//data collect:
-		for (int i=0; i<n; i++) {
-			System.out.println("Digite os dados do " + (i+1) + "o contribuinte:");
+
+		// data collect:
+		System.out.println();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Digite os dados do " + (i + 1) + "o contribuinte:");
 			System.out.print("Renda anual com salário: ");
 			double salaryIncome = sc.nextDouble();
 			System.out.print("Renda anual com prestação de serviço: ");
@@ -32,18 +33,27 @@ public class Program {
 			double healthSpending = sc.nextDouble();
 			System.out.print("Gastos educacionais: ");
 			double educationSpending = sc.nextDouble();
-			
+
 			list.add(new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending));
+			System.out.println();
 		}
-		
-		//calculation:
-		
-		
-		
-		//report:
-		
-		
+
+		// calculation and report:
+		int cont = 1;
+		for (TaxPayer obj : list) {
+			System.out.println("Resumo do " + cont + "o contribuinte:");
+			for (int i = 0; i < n; i++) {
+				if (i + 1 < list.size()) {
+					System.out.printf("Imposto bruto total: %.2f%n", obj.grossTax());
+					System.out.printf("Abatimento: %.2f%n", obj.taxRebate());
+					System.out.printf("Imposto devido: %.2f%n", obj.netTax());
+					System.out.println();
+					cont++;
+					break;
+				}
+			}
+		}
+
 		sc.close();
 	}
-
 }
--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -1,13 +1,16 @@
 package entities;
 
 public class TaxPayer {
-	
+
+	private static final double SERVICE_TAX = 0.15;
+	private static final double CAPITAL_TAX = 0.2;
+
 	private Double salaryIncome;
 	private Double servicesIncome;
 	private Double capitalIncome;
 	private Double healthSpending;
 	private Double educationSpending;
-	
+
 	public TaxPayer() {
 	}
 
@@ -57,5 +60,52 @@ public class TaxPayer {
 
 	public void setEducationSpending(Double educationSpending) {
 		this.educationSpending = educationSpending;
+	}
+
+	public Double salaryTax() {
+		double salaryMonth = this.salaryIncome / 12.0;
+		if (salaryMonth < 3000.0) {
+			return 0.0;
+		} 
+		else if (salaryMonth < 5000.0) {
+			return this.salaryIncome * 0.1;
+		} 
+		else {
+			return this.salaryIncome * 0.2;
+		}
+	}
+
+	public Double serviceTax() {
+		return this.servicesIncome * SERVICE_TAX;
+	}
+
+	public Double capitalTax() {
+		return this.capitalIncome * CAPITAL_TAX;
+	}
+
+	public Double grossTax() {
+		return salaryTax() + serviceTax() + capitalTax();
+	}
+
+	public Double taxRebate() {
+		double ceiling = grossTax() * 0.3;
+		double rebate = this.healthSpending + this.educationSpending;
+		if (rebate > ceiling) {
+			return ceiling;
+		} 
+		else {
+			return rebate;
+		}
+	}
+
+	public Double netTax() {
+		return grossTax() - taxRebate();
+	}
+
+	@Override
+	public String toString() {
+		return "TaxPayer [salaryIncome=" + salaryIncome + ", servicesIncome=" + servicesIncome + ", capitalIncome="
+				+ capitalIncome + ", healthSpending=" + healthSpending + ", educationSpending=" + educationSpending
+				+ "]";
 	}
 }
